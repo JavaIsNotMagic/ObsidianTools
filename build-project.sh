c04f9efc275a14acfe64d795d04828f14709b3d6
Connor <connor.tozer2017@gmail.com>
2018-12-02 18:50:18 -0500

    Build 2017 December 2nd RELEASE

--- build-project.sh
+++ build-project.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
+echo "Building HaxiumCore"
 /k/HaxiumCore/gradlew.bat build 1>&2> /dev/null
 case $? in
 0) echo "Project ObsidianTools built.";;
 1) exit 1;;
 esac
 cd "/k/HaxiumCore/build/libs"
-unzip $(find -name *.jar)
-f1=$(ls | grep "obsian_armor")
-f2=$(ls | grep "mcmod.info")
-if [ -e $f1 ]; then
-	mkdir ./assets
-	mv $f1 ./assets
-fi
-if [ -e $f2 ]; then
-	mv $f2 ./assets
-fi
-zip $(find -name com) $(find -name assets) ./ObsidianTools_1.7.10-latest.jar
+echo "Unpacking Build file"
+unzip $(find -name *.jar) 1>&2> /dev/null
+mkdir assets
+case $? in
+0);;
+1) echo "Direcory exists.";;
+esac
+mv -f obsidian_armor mcmod.info assets 1>&2> /dev/null
 case $? in
 0);;
 1) echo "Error."; exit 1;;
 esac
-rm -rf com assets
-mv ./ObsidianTools_1.7.10-latest.jar /c/Users/Connor\ Tozer/AppData/Roaming/.minecraft/mods
+echo "Packing Mod"
+/k/Programs/bash/zip -r ./ObsidianTools_1.7.10-latest.jar $(find -name com) $(find -name assets) 1>&2> /dev/null
+case $? in
+0);;
+1) echo "Error."; exit 1;;
+esac
+echo "Moving mod to %AppData%\.minecraft\.mods"
+mv -f ./ObsidianTools_1.7.10-latest.jar /c/Users/Connor\ Tozer/AppData/Roaming/.minecraft/mods 1>&2> /dev/null
 case $? in
 0);;
 1) echo "Error."; exit 1;;
 esac
 echo "Mod added to mod list"
+rm -rf com assets META-INF
