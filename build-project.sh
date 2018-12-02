e8693ff759ab56eceb4f0ac6f16bbacb4720c98f
Connor <connor.tozer2017@gmail.com>
2018-12-02 14:47:15 -0500

    Update

--- build-project.sh
+++ build-project.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
-/home/connor/Desktop/ME-Files/gradlew build 1>&2> /dev/null
+/k/HaxiumCore/gradlew.bat build 1>&2> /dev/null
 case $? in
 0) echo "Project ObsidianTools built.";;
 1) exit 1;;
 esac
-cp -f /home/connor/Desktop/ME-Files/build/libs/ObsidianTools-1.7.10-1.0.34.jar /home/connor/.minecraft/mods
+cd "/k/HaxiumCore/build/libs"
+unzip $(find -name *.jar)
+f1=$(ls | grep "obsian_armor")
+f2=$(ls | grep "mcmod.info")
+if [ -e $f1 ]; then
+	mkdir ./assets
+	mv $f1 ./assets
+fi
+if [ -e $f2 ]; then
+	mv $f2 ./assets
+fi
+zip $(find -name com) $(find -name assets) ./ObsidianTools_1.7.10-latest.jar
 case $? in
-0) echo "ObsidianTools added to mod list";;
-1) exit 1;;
+0);;
+1) echo "Error."; exit 1;;
 esac
-
+rm -rf com assets
+mv ./ObsidianTools_1.7.10-latest.jar /c/Users/Connor\ Tozer/AppData/Roaming/.minecraft/mods
+case $? in
+0);;
+1) echo "Error."; exit 1;;
+esac
+echo "Mod added to mod list"
